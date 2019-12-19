c472933ef637b295b0ffbe491621bb20380f0be5
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-19 16:47:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   
-  attr_accessor :brand, :color, :size, :material, :condition
-  attr_reader :new 
+  attr_accessor :brand, :color, :size, :material, :condition :new
+   
   def initialize(brand)
     @brand = brand 
   end 
